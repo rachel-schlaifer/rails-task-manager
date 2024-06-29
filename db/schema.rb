beba38cd7d7d95bf3ad49d6edbd20919dbbd2d84
Rachel Schlaifer <rachelschlaifer9@gmail.com>
2024-06-29 15:47:50 +0900

    Built the to-do list logic

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_29_031410) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
